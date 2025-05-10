41d4d9d581b6ad2a17c435ddf7e04b0c6c62b448
joohnv <johnv.aguayo@gmail.com>
2025-05-10 21:26:55 +0200

    eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Activa el modo estricto de React
+  eslint: {
+    ignoreDuringBuilds: true, 
+  },
 };
 
 export default nextConfig;
